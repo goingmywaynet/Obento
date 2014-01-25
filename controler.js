5a62f4a6183c8d8d753ac244db4371f2c29b33f4
Joey Chen <joey@goingmyway.net>
2014-01-25 15:45:07 +0900

    としゆきさんお試し中

--- controler.js
+++ controler.js
@@ -1,6 +1,11 @@
 // -- DOM準備完了後開始処理 (MAIN) --
 $(document).ready(function(){
 
+// function name(){
+//   var a = new String("");
+//   a = "てすと人";
+//   $('.n1').text(a);
+// }
   updateDate();
 
 function updateDate(){ // -- 時計(日時曜日)を更新する --
@@ -14,11 +19,22 @@ function updateDate(){ // -- 時計(日時曜日)を更新する --
   var clockText = yy + "/" + mm + "/" + dd + " (" + day[date.getDay()] +") " + HH + ":" + MM;
   $('.today').html(clockText);
 }
-  
+
+//---------------------  
   $("#System").click( function(){
-    $(".UserChoicePane").append(
-      "<div class=\"UserName\">test</div>"
-      );
+//    $(".UserChoicePane").append(
+//      "<div class=\"UserName\">test</div>"
+//      );
+   $(".UserChoicePane").append(
+	"<FORM class=\"myForm\"><INPUT TYPE=button VALUE=\"TEXT\" NAME=\"object\" ONCLICK=expression> </FORM>"
+    );
+
   });
+//--------------------
 
 });
+
+
+
+
+
