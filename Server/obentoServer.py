d404dc8065e7f4579888433d423a098259a08493
Joey Chen <joey@goingmyway.net>
2014-08-03 19:37:05 +0900

    obentoServer.py にデバッグ用関数追加

--- Server/obentoServer.py
+++ Server/obentoServer.py
@@ -4,15 +4,31 @@
 # Obento Project
 # - XML-RPC Server Side Program -
 #
+import inspect  # for debug
 
+#
+# Configure
+#
+mydb = '../DB/ObentoDB.db' # DB File
+debugOn = True             # print debug text
+
+#
+# Debug用関数
+#
+def debugFunc(datas):
+    if debugOn:
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        print u"=========\t" , calframe[1][3], "\t========="
+        for data in datas:
+            print u"DATA> ", repr(data).decode('unicode-escape')
+
+#
 # Obento XML-RPC Server
+#
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 import sqlite3
-
-#
-# XML-RPC Server
-#
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -39,7 +55,9 @@ server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
 
-
+#
+# SAMPLE CODE
+#
 # Register pow() function; this will use the value of
 # pow.__name__ as the name, which is just 'pow'.
 # server.register_function(pow)
@@ -61,15 +79,15 @@ server.register_introspection_functions()
 # Server側処理実装
 #
 
-mydb = '../DB/ObentoDB.db'
-
 # M_section 取得
 def SelectM_section():
     db = sqlite3.connect( mydb, isolation_level=None)
     sql = u"SELECT id, name FROM M_section"
     cursor = db.execute(sql)
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_section,'getM_section_ALL')
 
 # M_user 取得
@@ -77,8 +95,10 @@ def SelectM_user(flag):
     db = sqlite3.connect( mydb, isolation_level=None)
     sql = u"SELECT id, section_id, name FROM M_user WHERE enable_flag = ?"
     cursor = db.execute(sql,(flag,))
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_user,'getM_user_FLAG')
 
 # M_shop 取得
@@ -86,8 +106,10 @@ def SelectM_shop(flag):
     db = sqlite3.connect( mydb, isolation_level=None)
     sql = u"SELECT id, name FROM M_shop WHERE enable_flag = ?"
     cursor = db.execute(sql,(flag,))
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_shop,'getM_shop_FLAG')
 
 # M_menu 取得
@@ -95,8 +117,10 @@ def SelectM_menu(shop_id,flag):
     db = sqlite3.connect( mydb, isolation_level=None)
     sql = u"SELECT id, name ,price FROM M_menu WHERE shop_id = ? AND enable_flag = ?"
     cursor = db.execute(sql,(shop_id,flag) )
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_menu,'getM_menu_SHOP_FLAG')
 
 # M_option_group 取得
@@ -106,14 +130,12 @@ def SelectM_option_group(menu_id):
     cursor = db.execute(sql,(menu_id,))
     r = cursor.fetchall()
     if len(r) > 0:
-        return r
+        res = r
     else:
-        return [(0,0)]
-    #Debug
-    # print r
-    # print len(r)
-    # return cursor.fetchall()
+        res = [(0,0)]
     db.close()
+    debugFunc([sql,res,r,len(r)])
+    return res
 server.register_function(SelectM_option_group,'getM_option_group_MENU')
 
 # M_option 取得
@@ -121,8 +143,10 @@ def SelectM_option_GROUP(group_id):
     db = sqlite3.connect( mydb, isolation_level=None)
     sql = u"SELECT id,name FROM M_option WHERE group_id = ?"
     cursor = db.execute(sql,(group_id,))
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_option_GROUP,'getM_option_GROUP')
 
 # M_option 取得
@@ -131,8 +155,10 @@ def SelectM_option_MENU(menu_id):
     sql = u"SELECT id,name FROM M_option WHERE group_id = (SELECT group_id FROM M_option_group WHERE menu_id=?)"
     cursor = db.execute(sql,(menu_id,)) 
     #menu_idが2桁以上の場合 (menu_id) だと、2件とカウントされしまうので注意
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectM_option_MENU,'getM_option_MENU')
 
 # T_ORDER への INSERT
@@ -141,6 +167,7 @@ def InsertT_ORDER(order_date,user_id,menu_id,payment):
     sql = u"INSERT INTO T_ORDER (order_date, user_id, menu_id, payment) VALUES (?, ?, ?, ?)"
     db.execute(sql, (order_date,user_id,menu_id,payment))
     db.close()
+    debugFunc([sql, (order_date,user_id,menu_id,payment)])
     return 1
 server.register_function(InsertT_ORDER,'T_ORDER-INSERT')
 
@@ -150,6 +177,7 @@ def InsertT_order_option(order_id,option_id):
     sql = u"INSERT INTO T_ORDER (order_date, user_id, menu_id, payment) VALUES (?, ?, ?, ?)"
     db.execute(sql, (order_date,user_id,menu_id,payment))
     db.close()
+    debugFunc([sql, (order_date,user_id,menu_id,payment)])
     return 1
 server.register_function(InsertT_ORDER,'T_ORDER-INSERT')
 
@@ -162,12 +190,11 @@ def SelectT_ORDER():
     # print cursor
     # for row in cursor:
     #     print row
-    return cursor.fetchall()
+    res = cursor.fetchall()
     db.close()
-    # return 1
+    debugFunc([sql,res])
+    return res
 server.register_function(SelectT_ORDER,'T_ORDER-SELECT-ALL')
-
-
 
 
 # Run the server's main loop
